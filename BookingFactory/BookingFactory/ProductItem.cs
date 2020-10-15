c719ac651e3c4e0d6e3b8e53f22af5829cfb41eb
Sergii Kliebanov <s.klebanov497@gmail.com>
2020-10-15 15:15:53 +0300

    Removed duplicates

--- BookingFactory/BookingFactory/ProductItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookingFactory
-{
-    public struct ProductItem
-    {
-        public int ProductId { get; set; }
-        public decimal Quantity { get; set; }
-        public decimal Price { get; set; }
-    }
-}
